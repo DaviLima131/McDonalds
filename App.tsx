f3251113999728cc7a499c15ca0501426afff393
DAVI LIMA OLIVEIRA <davi.1902@aluno.pr.senac.br>
2026-06-10 11:28:36 -0300

    Colocando conteúdo no HomeScreen do App.tsx e aplicando rotas no App.tsx para HomeScreen dentro da const

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen, { RootStackParamList } from './screens/HomeScreen'
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export default function App() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator
+             initialRouteName='Home'
+                    screenOptions={{ headerShown: false }}
+            >                                                                           
+                <Stack.Screen name='Home' component={HomeScreen} />                                    
+                   
+            </Stack.Navigator>
+        </NavigationContainer>             
+    )                           
+}               
